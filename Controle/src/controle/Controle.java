c88d06be099c33a8f4e3010b1b8731b8de2753db
Oséias Barreto <166327083+OseiasBarreto@users.noreply.github.com>
2024-10-31 20:46:30 -0300

    Add files via upload
    Exercício de encapsulamento

---
+++ Controle/src/controle/Controle.java
@@ -0,0 +1,16 @@
+
+package controle;
+
+public class Controle {
+
+    public static void main(String[] args) {
+       Controle_remoto c = new Controle_remoto();
+      
+      c.ligar();
+      c.mais_volume();
+      c.mais_volume();  
+      c.desligar_mutar();
+      c.abrir_menu();
+    }
+    
+}
