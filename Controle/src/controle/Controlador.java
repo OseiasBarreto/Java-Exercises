8a00c8dd5592954d339fdfb9a820056d98b590f4
Oséias Barreto <166327083+OseiasBarreto@users.noreply.github.com>
2024-10-31 20:42:55 -0300

    Exercício de encapsulamento
    Exercício de encapsulamento

---
+++ Controle/src/controle/Controlador.java
@@ -0,0 +1,14 @@
+
+package controle;
+public interface Controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrir_menu();
+    public abstract void fechar_menu();
+    public abstract void mais_volume();
+    public abstract void menos_volume();
+    public abstract void ligar_mutar();
+    public abstract void desligar_mutar();
+    public abstract void play();
+    public abstract void pause();
+}
